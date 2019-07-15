061467b334aba05b757d9ca0384b9017f52838a9
Draugr scourge Fisher <dfisher@at-q2.21-school.ru>
2019-07-15 12:40:04 +0300

    all good, except for lst_map, strtim && strstr

--- libft-my-tests/memleaks.c
+++ libft-my-tests/memleaks.c
@@ -6,7 +6,7 @@
 /*   By: dfisher <dfisher@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/07/13 18:12:59 by dfisher           #+#    #+#             */
-/*   Updated: 2019/07/13 22:08:21 by dfisher          ###   ########.fr       */
+/*   Updated: 2019/07/15 12:38:53 by dfisher          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -55,41 +55,71 @@ void	ft_lstdelone_test(t_list **alst, void (*del)(void *, size_t))
 }
 */
 
-void	ft_lstdelone_test2(t_list *alst, void (*del)(void *, size_t))
+void	ft_bzero_test(void *s, size_t n)
 {
-	printf("....Hmm.....\n");
-	if (alst)
-		printf("%s\n", alst->content);
-		alst->content = "AAAAAA";
-		printf("%s\n", alst->content);
-	printf("....Hmm.....\n");
-	printf("alst adress		%p\n", alst);
-	printf("*alst adress (it is == to &of main list)	%p\n", alst);
-	printf("alst content		%s\n", alst->content);
-	printf("alst content_size		%zu\n", alst->content_size);
-	/*printf("alst content_adress	%p\n", (**alst).content);
-	printf("alst content_size		%zu\n", (*alst)->content_size);
+	printf("## START CLEANING\n");
+	printf("## content_size =  %lu\n", n);
+	unsigned char	*b_uc;
 
-	printf("L2 adress		%p\n", test->next);
-	printf("L2 content		%s\n", test->next->content);
-	printf("L2 content_size		%zu\n", test->next->content_size);
-	printf("L3 adress		%p\n", test->next->next);
-	printf("L3 content		%s\n", test->next->next->content);
-	printf("L3 content_size		%zu\n", test->next->next->content_size);*/
-	if (!alst)
+	b_uc = (unsigned char *)s;
+	printf("## s == %s\n" , s);
+	printf("## MIDDLE\n");
+	while (n--)
+		*b_uc++ = (unsigned char)0;
+	printf("## ZEROING IS FINISHED\n");
+}
+
+
+void	ft_lstdel_test(t_list **alst, void (*del)(void *, size_t))
+{
+	t_list *next;
+
+	printf("## BEFORE DEL\n");
+	printf("## alst adress					%p\n", alst);
+	printf("## *alst adress (it is == to &of main list)	%p\n", *alst);
+	printf("## alst content adress				%p\n", (*alst)->content);
+	printf("## alst content					%s\n", (*alst)->content);
+	printf("## alst content_size				%zu\n", (*alst)->content_size);
+	printf("## alst content_adress				%p\n", (**alst).content);
+	printf("## alst content_size				%zu\n", (*alst)->content_size);
+	if (alst && *alst && del)
 	{
-		del((alst)->content, (alst)->content_size);
-		free(alst);
-		alst = NULL;
+		printf("###### START IF\n");
+		while (alst && *alst && del)
+		{
+			printf("#### START WHILE\n");
+			next = (*alst)->next;
+			del((*alst)->content, (*alst)->content_size);
+			free(*alst);
+			*alst = next;
+		}
+		*alst = NULL;
 	}
 }
 
-void	*ft_lst_bzero(void *s, size_t n)
-{
-	printf("COME ON here\n");
-	return(ft_memset(s, 0, n));
-}
 
+void	ft_lstdelone_test(t_list **alst, void (*del)(void *, size_t))
+{
+	printf("## BEFORE DEL\n");
+	printf("## alst adress					%p\n", alst);
+	printf("## *alst adress (it is == to &of main list)	%p\n", *alst);
+	printf("## alst content adress				%p\n", (*alst)->content);
+	printf("## alst content					%s\n", (*alst)->content);
+	printf("## alst content_size				%zu\n", (*alst)->content_size);
+	printf("## alst content_adress				%p\n", (**alst).content);
+	printf("## alst content_size				%zu\n", (*alst)->content_size);
+	if (alst && *alst)
+	{
+		printf("###### START IF\n");
+
+		del((*alst)->content, (*alst)->content_size);
+		free(*alst);
+		*alst = NULL;
+	}
+	printf("## FINISHED DEL\n");
+	printf("AFTER DEL\n");
+	printf("alst adress		%p\n", alst);
+}
 
 int	main(void)
 {
@@ -156,8 +186,58 @@ int	main(void)
 	printf("L3 content		%s\n", test->next->next->content);
 	printf("L3 content_size		%zu\n", test->next->next->content_size);
 	printf("L4 adress		%p\n", test->next->next->next);
-	printf("NOW ALL LEVELS HAVE DIFFERENT CONTENT\n");
+	printf("CHANGE L2\n");
 	ft_change_content_once(test->next->next);
+	printf("L1 adress		%p\n", test);
+	printf("L1 content		%s\n", test->content);
+	printf("L1 content adress	%p\n", test->content);
+	printf("L1 content_size		%zu\n", test->content_size);
+	printf("L1 content_size adress	%p\n", &test->content_size);
+	printf("L2 adress		%p\n", test->next);
+	printf("L2 content		%s\n", test->next->content);
+	printf("L1 content adress	%p\n", test->next->content);
+	printf("L2 content_size		%zu\n", test->next->content_size);
+	printf("L3 adress		%p\n", test->next->next);
+	printf("L3 content		%s\n", test->next->next->content);
+	printf("L1 content adress	%p\n", test->next->next->content);
+	printf("L3 content_size		%zu\n", test->next->next->content_size);
+	printf("L4 adress		%p\n", test->next->next->next);
+	printf("NOW ALL LEVELS HAVE DIFFERENT CONTENT\n");
+	t_list *test_del = ft_lstnew(sample_content, content_size);
+	t_list *test_del_new_elem = ft_lstnew(sample_content, content_size);
+	t_list *test_del_new_elem_2 = ft_lstnew(sample_content, content_size);
+	ft_lstadd(&test_del, test_del_new_elem);
+	ft_lstadd(&test_del, test_del_new_elem_2);
+	printf("LIST TO DEL\n");
+	printf("L1 adress		%p\n", test_del);
+	printf("L1 content		%s\n", test_del->content);
+	printf("L1 content adress	%p\n", &test_del->content);
+	printf("L1 content_size		%zu\n", test_del->content_size);
+	printf("L1 content_size adress	%p\n", &test_del->content_size);
+	printf("L2 adress		%p\n", test_del->next);
+	printf("L2 content		%s\n", test_del->next->content);
+	printf("L2 content adress	%p\n", &test_del->next->content);
+	printf("L2 content_size		%zu\n", test_del->next->content_size);
+	printf("L3 adress		%p\n", test_del->next->next);
+	printf("L3 content		%s\n", test_del->next->next->content);
+	printf("L3 content adress	%p\n", &test_del->next->next->content);
+	printf("L3 content_size		%zu\n", test_del->next->next->content_size);
+	printf("L4 adress		%p\n", test_del->next->next->next);
+	ft_lstdelone_test(&test_del->next, ft_bzero_test);
+	printf("AFTER DEL\n");
+	printf("L1 adress		%p\n", test_del);
+	printf("L1 content		%s\n", test_del->content);
+	printf("L1 content adress	%p\n", &test_del->content);
+	printf("L1 content_size		%zu\n", test_del->content_size);
+	printf("L1 content_size adress	%p\n", &test_del->content_size);
+	//printf("L2 adress		%p\n", test_del->next);
+	printf("L3 adress		%p\n", test_del->next->next);
+	printf("L3 content		%s\n", test_del->next->next->content);
+	printf("L3 content adress	%p\n", &test_del->next->next->content);
+	printf("L3 content_size		%zu\n", test_del->next->next->content_size);
+	printf("L4 adress		%p\n", test_del->next->next->next);
+	/*ft_lstdelone_test(&(test->next), ft_bzero);
+	printf("AFTER DEL\n");
 	printf("L1 adress		%p\n", test);
 	printf("L1 content		%s\n", test->content);
 	printf("L1 content_size		%zu\n", test->content_size);
@@ -167,18 +247,7 @@ int	main(void)
 	printf("L3 adress		%p\n", test->next->next);
 	printf("L3 content		%s\n", test->next->next->content);
 	printf("L3 content_size		%zu\n", test->next->next->content_size);
-	printf("L4 adress		%p\n", test->next->next->next);
-	printf("NOW ALL LEVELS HAVE DIFFERENT CONTENT\n");
-	ft_lstdelone_test2(test, ft_lst_bzero(test, content_size));
-	/*printf("L1 adress		%p\n", test);
-	printf("L1 content		%s\n", test->content);
-	printf("L1 content_size		%zu\n", test->content_size);
-	printf("L2 adress		%p\n", test->next);
-	printf("L2 content		%s\n", test->next->content);
-	printf("L2 content_size		%zu\n", test->next->content_size);
-	printf("L3 adress		%p\n", test->next->next);
-	printf("L3 content		%s\n", test->next->next->content);
-	printf("L3 content_size		%zu\n", test->next->next->content_size);*/
+*/
 	//ft_lstiter(test, tolower(l));
 	//printf("%s", sample_content);
 	//printf("%s", sample_content);
